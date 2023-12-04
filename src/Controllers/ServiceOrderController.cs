0bbe47b39bb3e2779b895526a7bb5ba63b88ac70
fgandraf <fgandraf@gmail.com>
2023-12-04 15:38:18 -0300

    feat: Async Service Order

--- src/Controllers/ServiceOrderController.cs
+++ src/Controllers/ServiceOrderController.cs
@@ -16,8 +16,8 @@ namespace FluxusApi.Controllers
             => _authenticator = new Authentication(context);
 
 
-        [HttpGet("v1/services-orders/flow")]
-        public IActionResult GetOrdersFlow()
+        [HttpGet("v1/service-orders/flow")]
+        public async Task<IActionResult> GetOrdersFlow()
         {
             try
             {
@@ -25,7 +25,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetOrdersFlow();
+                    result = await new ServiceOrderRepository(connection).GetOrdersFlowAsync();
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -36,8 +36,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/services-orders/cities")]
-        public IActionResult GetOrderedCities()
+        [HttpGet("v1/service-orders/cities")]
+        public async Task<IActionResult> GetOrderedCities()
         {
             try
             {
@@ -45,7 +45,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetOrderedCities();
+                    result = await new ServiceOrderRepository(connection).GetOrderedCitiesAsync();
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -56,8 +56,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/services-orders/done-to-invoice")]
-        public IActionResult GetDoneToInvoice() 
+        [HttpGet("v1/service-orders/done-to-invoice")]
+        public async Task<IActionResult> GetDoneToInvoice() 
         {
             try
             {
@@ -65,7 +65,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetDoneToInvoice();
+                    result = await new ServiceOrderRepository(connection).GetDoneToInvoiceAsync();
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -76,8 +76,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/services-orders/filtered/{filter}")]
-        public IActionResult GetFiltered(string filter)
+        [HttpGet("v1/service-orders/filtered/{filter}")]
+        public async Task<IActionResult> GetFiltered(string filter)
         {
             try
             {
@@ -85,7 +85,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetFiltered(filter);
+                    result = await new ServiceOrderRepository(connection).GetFilteredAsync(filter);
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -96,8 +96,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/services-orders/invoiced/{invoiceId}")]
-        public IActionResult GetInvoiced(int invoiceId)
+        [HttpGet("v1/service-orders/invoiced/{invoiceId}")]
+        public async Task<IActionResult> GetInvoiced(int invoiceId)
         {
             try
             {
@@ -105,7 +105,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetInvoiced(invoiceId);
+                    result = await new ServiceOrderRepository(connection).GetInvoicedAsync(invoiceId);
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -116,8 +116,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/services-orders/professionals/{invoiceId}")]
-        public IActionResult GetProfessionals(int invoiceId)
+        [HttpGet("v1/service-orders/professionals/{invoiceId}")]
+        public async Task<IActionResult> GetProfessionals(int invoiceId)
         {
             try
             {
@@ -125,7 +125,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).GetProfessional(invoiceId);
+                    result = await new ServiceOrderRepository(connection).GetProfessionalAsync(invoiceId);
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -136,8 +136,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpGet("v1/service-order/{id}")]
-        public IActionResult Get(int id)
+        [HttpGet("v1/service-orders/{id}")]
+        public async Task<IActionResult> GetById(int id)
         {
             try
             {
@@ -145,7 +145,7 @@ namespace FluxusApi.Controllers
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    result = new ServiceOrderRepository(connection).Get(id);
+                    result = await new ServiceOrderRepository(connection).GetAsync(id);
 
                 return result == null ? NotFound() : Ok(result);
             }
@@ -156,8 +156,8 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpPost("v1/service-order")]
-        public IActionResult Post([FromBody] ServiceOrder serviceOrder)
+        [HttpPost("v1/service-orders")]
+        public async Task<IActionResult> Post([FromBody] ServiceOrder serviceOrder)
         {
             try
             {
@@ -165,7 +165,7 @@ namespace FluxusApi.Controllers
 
                 long id;
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    id = new ServiceOrderRepository(connection).Insert(serviceOrder);
+                    id = await new ServiceOrderRepository(connection).InsertAsync(serviceOrder);
 
                 return Ok(id);
             }
@@ -176,15 +176,15 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpPut("v1/service-order")]
-        public IActionResult Put([FromBody] ServiceOrder serviceOrder)
+        [HttpPut("v1/service-orders")]
+        public async Task<IActionResult> Put([FromBody] ServiceOrder serviceOrder)
         {
             try
             {
                 _authenticator.Authenticate();
 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    new ServiceOrderRepository(connection).Update(serviceOrder);
+                    await new ServiceOrderRepository(connection).UpdateAsync(serviceOrder);
 
                 return Ok();
             }
@@ -195,34 +195,15 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpPut("v1/service-order/update-invoice/{id},{invoice_id}")]
-        public IActionResult UpdateInvoiceId(int id, int invoiceId)
-        {
-            try
-            {
-                _authenticator.Authenticate();
-                
-                using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    new ServiceOrderRepository(connection).UpdateInvoiceId(id, invoiceId);
-
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-        }
-
-
-        [HttpPut("v1/service-order/update-status/{id},{status}")]
-        public IActionResult UpdateStatus(int id, EnumStatus status)
+        [HttpPut("v1/service-orders/update-invoice/{id},{invoice_id}")]
+        public async Task<IActionResult> UpdateInvoiceId(int id, int invoiceId)
         {
             try
             {
                 _authenticator.Authenticate();
                 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
-                    new ServiceOrderRepository(connection).UpdateStatus(id, status);
+                    await new ServiceOrderRepository(connection).UpdateInvoiceIdAsync(id, invoiceId);
 
                 return Ok();
             }
@@ -233,8 +214,27 @@ namespace FluxusApi.Controllers
         }
 
 
-        [HttpDelete("v1/service-order/{id}")]
-        public IActionResult Delete(int id)
+        [HttpPut("v1/service-orders/update-status/{id},{status}")]
+        public async Task<IActionResult> UpdateStatus(int id, EnumStatus status)
+        {
+            try
+            {
+                _authenticator.Authenticate();
+                
+                using (var connection = new MySqlConnection(_authenticator.ConnectionString))
+                    await new ServiceOrderRepository(connection).UpdateStatusAsync(id, status);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        [HttpDelete("v1/service-orders/{id}")]
+        public async Task<IActionResult> Delete(int id)
         {
             try
             {
@@ -243,10 +243,10 @@ namespace FluxusApi.Controllers
                 
                 using (var connection = new MySqlConnection(_authenticator.ConnectionString))
                 {
-                    var serviceOrder = new ServiceOrderRepository(connection).Get(id);
+                    var serviceOrder = await new ServiceOrderRepository(connection).GetAsync(id);
 
                     if (serviceOrder.Id != 0)
-                        deleted = new ServiceOrderRepository(connection).Delete(serviceOrder);
+                        deleted = await new ServiceOrderRepository(connection).DeleteAsync(serviceOrder);
 
                     return deleted == false ? NotFound() : Ok();
                 }
--- src/Repositories/ServiceOrderRepository.cs
+++ src/Repositories/ServiceOrderRepository.cs
@@ -37,6 +37,34 @@ namespace FluxusApi.Repositories
             return Connection.Query(query);
         }
         
+        public async Task<IEnumerable> GetOrdersFlowAsync()
+        {
+            string query = @"
+                SELECT 
+                    A.Id,
+                    CONCAT(
+                        B.Tag, '-', A.City, '-', 
+                        CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(A.ReferenceCode, '/', 1), '.', -1) AS UNSIGNED),
+                        '\n\n',
+                        REPLACE(A.CustomerName, ' ', ' '),
+                        '\n- Prazo: ', DATE_FORMAT(A.Deadline, '%d/%m/%Y')
+                    ) AS Title,
+                    A.Status, 
+                    A.ProfessionalId 
+                FROM 
+                    ServiceOrder as A 
+                INNER JOIN
+                    Service as B
+                ON
+                    B.Id = A.ServiceId
+                WHERE 
+                    InvoiceId = 0 
+                ORDER BY 
+                    OrderDate";
+
+            return await Connection.QueryAsync(query);
+        }
+        
 
         public IEnumerable GetInvoiced(int invoiceId)
         {
@@ -73,6 +101,42 @@ namespace FluxusApi.Repositories
 
             return Connection.Query(query, new { invoiceId });
         }
+        
+        public async Task<IEnumerable> GetInvoicedAsync(int invoiceId)
+        {
+            string query = @"
+                SELECT 
+                    os.Id, 
+                    os.OrderDate, 
+                    os.ReferenceCode, 
+                    os.ProfessionalId,
+                    pr.Tag Professional,
+                    sr.Tag Service,
+                    os.City, 
+                    os.CustomerName, 
+                    os.SurveyDate, 
+                    os.DoneDate, 
+                    os.InvoiceId, 
+                    os.Status, 
+                    os.ServiceAmount, 
+                    os.MileageAllowance 
+                FROM 
+                    ServiceOrder os
+                INNER JOIN
+                    Service sr
+                ON
+                    os.ServiceId = sr.Id
+                INNER JOIN
+                    Professional pr
+                ON
+                    os.ProfessionalId = pr.Id
+                WHERE 
+                    InvoiceId = @invoiceId
+                ORDER BY 
+                    DoneDate";
+
+            return await Connection.QueryAsync(query, new { invoiceId });
+        }
 
 
         public IEnumerable GetDoneToInvoice()
@@ -108,6 +172,41 @@ namespace FluxusApi.Repositories
                     DoneDate";
 
             return Connection.Query(query);
+        }
+        
+        public async Task<IEnumerable>GetDoneToInvoiceAsync()
+        {
+            string query = @"
+                SELECT 
+                    os.Id, 
+                    os.OrderDate, 
+                    os.ReferenceCode, 
+                    pr.Tag Professional,
+                    sr.Tag Service,
+                    os.City, 
+                    os.CustomerName, 
+                    os.SurveyDate,
+                    os.DoneDate, 
+                    os.ServiceAmount, 
+                    os.MileageAllowance
+                FROM 
+                    ServiceOrder os
+                INNER JOIN
+                    Service sr
+                ON
+                    os.ServiceId = sr.Id
+                INNER JOIN
+                    Professional pr
+                ON
+                    os.ProfessionalId = pr.Id
+                WHERE 
+                    Invoiced = 0
+                AND 
+                    Status = 4
+                ORDER BY 
+                    DoneDate";
+
+            return await Connection.QueryAsync(query);
         }
 
         public IEnumerable GetFiltered(string filter)
@@ -152,8 +251,71 @@ namespace FluxusApi.Repositories
 
             return Connection.Query(query, param);
         }
+        
+        public async Task<IEnumerable>GetFilteredAsync(string filter)
+        {
+            string query = @"
+                SELECT 
+                    os.Id,
+                    os.Status,
+                    pr.Tag Professional,
+                    os.OrderDate,
+                    os.ReferenceCode,
+                    sr.Tag Service,
+                    os.City,
+                    os.CustomerName,
+                    os.SurveyDate,
+                    os.DoneDate,
+                    os.Invoiced
+                FROM
+                    ServiceOrder os
+                INNER JOIN
+                    Service sr
+                ON
+                    os.ServiceId = sr.Id
+                INNER JOIN
+                    Professional pr
+                ON
+                    os.ProfessionalId = pr.Id
+                WHERE 
+                    pr.Tag LIKE @professional AND sr.Tag LIKE @service AND os.City LIKE @city AND os.Status LIKE @status AND os.Invoiced LIKE @invoiced
+                ORDER BY 
+                    OrderDate";
+
+            string[] filters = filter.Split(',');
+            var param = new
+            {
+                professional = filters[0],
+                service = filters[1],
+                city = filters[2],
+                status = filters[3],
+                invoiced = filters[4]
+            };
+
+            return await Connection.QueryAsync(query, param);
+        }
 
 
+        public async Task<IEnumerable> GetProfessionalAsync(int invoiceId)
+        {
+            string query = @"
+                SELECT DISTINCT 
+                    t1.ProfessionalId, 
+                    t2.Nameid 
+                FROM 
+                    ServiceOrder t1 
+                INNER JOIN 
+                    Professional t2 
+                on 
+                    t1.ProfessionalId = t2.Id 
+                WHERE 
+                    t1.InvoiceId = @invoiceId 
+                ORDER BY 
+                    t2.Nameid";
+
+            return await Connection.QueryAsync(query, new { invoiceId });
+        }
+        
         public IEnumerable GetProfessional(int invoiceId)
         {
             string query = @"
@@ -187,6 +349,19 @@ namespace FluxusApi.Repositories
 
             return Connection.Query(query);
         }
+        
+        public async Task<IEnumerable> GetOrderedCitiesAsync()
+        {
+            string query = @"
+                SELECT DISTINCT 
+                    City 
+                FROM 
+                    ServiceOrder 
+                ORDER BY 
+                    City";
+
+            return await Connection.QueryAsync(query);
+        }
 
         public int UpdateInvoiceId(int id, int invoiceId)
         {
@@ -212,9 +387,32 @@ namespace FluxusApi.Repositories
 
             return Connection.Execute(updateSQL, invoice);
         }
+        
+        public async Task<int> UpdateInvoiceIdAsync(int id, int invoiceId)
+        {
+            bool invoiced = invoiceId > 0;
+
+            string updateSql = @"
+                UPDATE
+                    ServiceOrder
+                SET
+                    InvoiceId = @invoiceId,
+                    Invoiced = @invoiced
+                WHERE
+                    Id = @Id";
+
+            var invoice = new
+            {
+                InvoiceId = invoiceId,
+                Invoiced = invoiced,
+                Id = id
+            };
+
+            return await Connection.ExecuteAsync(updateSql, invoice);
+        }
 
 
-        public int UpdateStatus(int id, EnumStatus status)
+        public async Task<int> UpdateStatusAsync(int id, EnumStatus status)
         {
             string changeDate = "";
             switch (status)
@@ -241,7 +439,9 @@ namespace FluxusApi.Repositories
                 Id = id
             };
 
-            return Connection.Execute(updateSQL, orderObj);
+            return await Connection.ExecuteAsync(updateSQL, orderObj);
         }
+        
     }
+
 }

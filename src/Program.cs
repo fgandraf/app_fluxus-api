047b6defc2bf4c21659c10a5e36b0b021ecef74d
fgandraf <fgandraf@gmail.com>
2023-12-19 19:03:59 -0300

    feat: Add ServiceExtensions

--- src/Program.cs
+++ src/Program.cs
@@ -1,53 +1,34 @@
-using FluxusApi.Repositories;
-using FluxusApi.Repositories.Contracts;
-using MySql.Data.MySqlClient;
+using FluxusApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//---Configurar Kestrel para ouvir em todas as interfaces
-builder.WebHost.UseKestrel(serverOptions =>
-    {
-        // Listen on port 5001 on all network interfaces.
-        serverOptions.ListenAnyIP(5001);
-    });
+// Configure Kestrel to listen on all interfaces on port 5001
+builder.WebHost.UseKestrel(serverOptions => serverOptions.ListenAnyIP(5001));
 
-
+// Adding and configuring MVC services for using Controllers
 builder.Services.AddControllers();
+
+// Swagger configuration for API documentation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => { c.OperationFilter<AddCustomHeader>(); });
 
-
-//---Adicionar Singleton
-
-builder.Services.AddScoped<MySqlConnection>(sp =>
-{
-    var configuration = sp.GetRequiredService<IConfiguration>();
-    var connectionString = configuration.GetConnectionString("Default");
-    return new MySqlConnection(connectionString);
-});
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddScoped<IBankBranchRepository, BankBranchRepository>();
-builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
-builder.Services.AddScoped<IProfessionalRepository, ProfessionalRepository>();
-builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
-builder.Services.AddScoped<IServiceOrderRepository, ServiceOrderRepository>();
-builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
-
+// Dependency Injection services configuration
+builder.Services.AddDatabaseServices(builder.Configuration);
+builder.Services.AddRepositoryServices();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Swagger UI configuration in development environment
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
---
+++ src/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+using FluxusApi.Repositories;
+using FluxusApi.Repositories.Contracts;
+using MySql.Data.MySqlClient;
+
+namespace FluxusApi;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddDatabaseServices(this IServiceCollection services, IConfiguration configuration) 
+        => services.AddScoped<MySqlConnection>(sp => 
+            new MySqlConnection(configuration.GetConnectionString("Default")));
+
+    public static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBankBranchRepository, BankBranchRepository>();
+        services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+        services.AddScoped<IProfessionalRepository, ProfessionalRepository>();
+        services.AddScoped<IProfileRepository, ProfileRepository>();
+        services.AddScoped<IServiceOrderRepository, ServiceOrderRepository>();
+        services.AddScoped<IServiceRepository, ServiceRepository>();
+
+        return services;
+    }
+}
